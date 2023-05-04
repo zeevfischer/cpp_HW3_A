f314baef483aeca90f27625de38e4f125266b396
zeevfischer <92921822+zeevfischer@users.noreply.github.com>
2023-05-04 16:41:25 +0300

    Add files via upload

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -9,6 +9,23 @@
 using namespace std;
 namespace ariel
 {
+    void Fraction::gcd()
+    {
+        int a = this->numerator;
+        int b = this->denominator;
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        int gcd = abs(a);
+        if(gcd != 0)
+        {
+            this->numerator/=gcd;
+            this->denominator/=gcd;
+        }
+    }
     //Note this will be numerator/denominator
     // constructor
     Fraction::Fraction(int numerator, int denominator)
@@ -20,85 +37,33 @@ namespace ariel
         this->numerator = numerator;
         this->denominator = denominator;
     }
-    // Fraction::Fraction(Fraction& temp)
-    // {
-    //     this->numerator = temp.numerator;
-    //     this->denominator = temp.denominator;
-    // }
-    // destructor
-    Fraction::~Fraction() {}
-    int Fraction::gcd() const
+    Fraction::Fraction(float num)
     {
-        int a = numerator;
-        int b = denominator;
-        while (b != 0)
+        this->numerator = num*1000;
+        this->denominator = 1000;
+        if(this->numerator != 0)
         {
-            int temp = b;
-            b = a % b;
-            a = temp;
+            this->gcd();
         }
-        return abs(a);
+    }
+    Fraction::Fraction()
+    {
+        this->numerator = 0;
+        this->denominator = 1;
     }
     // overloaded operators
-    Fraction operator+ (const Fraction& fraction1, const Fraction& fraction2)
-    {
-        // int new_numerator = fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator;
-        // int new_denominator = fraction1.denominator * fraction2.denominator;
-        // Fraction temp = Fraction(new_numerator, new_denominator);
-        // int gcd = temp.gcd();
-        // return Fraction(new_numerator/gcd, new_denominator/gcd);
-        return Fraction(1,1);
-    }
-    Fraction operator- (const Fraction& fraction1, const Fraction& fraction2)
-    {
-        // int new_numerator = fraction1.numerator * fraction2.denominator - fraction2.numerator * fraction1.denominator;
-        // int new_denominator = fraction1.denominator * fraction2.denominator;
-        // Fraction temp = Fraction(new_numerator, new_denominator);
-        // int gcd = temp.gcd();
-        // return Fraction(new_numerator/gcd, new_denominator/gcd);
-        return Fraction(1,1);
-    }
-    Fraction operator* (const Fraction& fraction1, const Fraction& fraction2)
-    {
-        // int new_numerator = fraction1.numerator *  fraction2.numerator;
-        // int new_denominator = fraction1.denominator * fraction2.denominator;
-        // Fraction temp = Fraction(new_numerator, new_denominator);
-        // int gcd = temp.gcd();
-        // return Fraction(new_numerator/gcd, new_denominator/gcd);
-        return Fraction(1,1);
-    }
-    Fraction operator/ (const Fraction& fraction1, const Fraction& fraction2)
-    {
-        // int new_numerator = fraction1.numerator *  fraction2.denominator;
-        // int new_denominator = fraction1.denominator * fraction2.numerator;
-        // Fraction temp = Fraction(new_numerator, new_denominator);
-        // int gcd = temp.gcd();
-        // return Fraction(new_numerator/gcd, new_denominator/gcd);
-        return Fraction(1,1);
-    }
+    Fraction operator+ (const Fraction& fraction1, const Fraction& fraction2){return Fraction(1,1);}
+    Fraction operator- (const Fraction& fraction1, const Fraction& fraction2){return Fraction(1,1);}
+    Fraction operator* (const Fraction& fraction1, const Fraction& fraction2){return Fraction(1,1);}
+    Fraction operator/ (const Fraction& fraction1, const Fraction& fraction2){return Fraction(1,1);}
+
     Fraction operator+ (const Fraction& fraction1, const float num1){return Fraction(1,1);}
     Fraction operator- (const Fraction& fraction1, const float num1){return Fraction(1,1);}
     Fraction operator* (const Fraction& fraction1, const float num1){return Fraction(1,1);}
     Fraction operator/ (const Fraction& fraction1, const float num1){return Fraction(1,1);}
 
-    Fraction operator+ (const float num1,const Fraction& fraction1){return Fraction(1,1);}
-    Fraction operator- (const float num1,const Fraction& fraction1){return Fraction(1,1);}
-    Fraction operator* (const float num1,const Fraction& fraction1){return Fraction(1,1);}
-    Fraction operator/ (const float num1,const Fraction& fraction1){return Fraction(1,1);}
 
-    bool operator==(const Fraction& fraction1, const Fraction& fraction2)
-    {
-        // if(fraction1.numerator == fraction2.numerator && fraction1.denominator == fraction2.denominator)
-        // {
-        //     return true;
-        // }
-        // else if((double)fraction1 == (double)fraction2)
-        // {
-        //     return true;
-        // }
-        // return false;
-        return false;
-    }
+    bool operator==(const Fraction& fraction1, const Fraction& fraction2){return false;}
     bool operator!=(const Fraction& fraction1, const Fraction& fraction2){return false;}
     bool operator>(const Fraction& fraction1, const Fraction& fraction2){return false;}
     bool operator<(const Fraction& fraction1, const Fraction& fraction2){return false;}
@@ -112,13 +77,6 @@ namespace ariel
     bool operator>=(const Fraction& fraction1, const float num1){return false;}
     bool operator<=(const Fraction& fraction1, const float num1){return false;}
 
-    bool operator==(const float num1, const Fraction& fraction1){return false;}
-    bool operator!=(const float num1, const Fraction& fraction1){return false;}
-    bool operator>(const float num1, const Fraction& fraction1){return false;}
-    bool operator<(const float num1, const Fraction& fraction1){return false;}
-    bool operator>=(const float num1, const Fraction& fraction1){return false;}
-    bool operator<=(const float num1, const Fraction& fraction1){return false;}
-
     // prefix
     Fraction& Fraction::operator++(){return *this;}
     // postfix
@@ -130,14 +88,10 @@ namespace ariel
     //check this is right !!!!
     istream& operator>>(istream& is, Fraction& fraction)
     {
-        // Fraction temp;
-        // char c;
-        // is >> fraction.numerator >> c >> fraction.denominator;
         return is;
     }
     ostream& operator<<(ostream& os, const Fraction& fraction)
     {
-	    // return os << fraction.numerator << '/' << fraction.denominator;
         return os;
     }
 }
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -18,67 +18,46 @@ namespace ariel
     public:
         // constructor
         Fraction(int num, int den);//: numerator(num), denominator(den){};
-        Fraction(const Fraction& temp);
+        Fraction();
+        /*
+            when a float will apear first this will turn it into a fraction
+            then it will send it to the operator of fraction x fraction 
+        */
+       // float conversion constructor
         Fraction(float num);
-        // destructor
-        ~Fraction();
-        int gcd() const;
+        void gcd();
         // overloaded operators
-        friend Fraction operator+ (const Fraction& fraction1, const Fraction& fraction2);//good
-        friend Fraction operator- (const Fraction& fraction1, const Fraction& fraction2);//good
-        friend Fraction operator* (const Fraction& fraction1, const Fraction& fraction2);//good
-        friend Fraction operator/ (const Fraction& fraction1, const Fraction& fraction2);//good
+        friend Fraction operator+ (const Fraction& fraction1, const Fraction& fraction2);
+        friend Fraction operator- (const Fraction& fraction1, const Fraction& fraction2);
+        friend Fraction operator* (const Fraction& fraction1, const Fraction& fraction2);
+        friend Fraction operator/ (const Fraction& fraction1, const Fraction& fraction2);
 
-        friend Fraction operator+ (const Fraction& fraction1, const float num1);
-        friend Fraction operator- (const Fraction& fraction1, const float num1);
-        friend Fraction operator* (const Fraction& fraction1, const float num1);
-        friend Fraction operator/ (const Fraction& fraction1, const float num1);
+        friend Fraction operator+ (const Fraction& fraction1, float num1);
+        friend Fraction operator- (const Fraction& fraction1, float num1);
+        friend Fraction operator* (const Fraction& fraction1, float num1);
+        friend Fraction operator/ (const Fraction& fraction1, float num1);
 
-        friend Fraction operator+ (const float num1,const Fraction& fraction1); 
-        friend Fraction operator- (const float num1,const Fraction& fraction1);
-        friend Fraction operator* (const float num1,const Fraction& fraction1);
-        friend Fraction operator/ (const float num1,const Fraction& fraction1);
-
-        friend bool operator==(const Fraction& fraction1, const Fraction& fraction2);//good
+        friend bool operator==(const Fraction& fraction1, const Fraction& fraction2);
         friend bool operator!=(const Fraction& fraction1, const Fraction& fraction2);
         friend bool operator>(const Fraction& fraction1, const Fraction& fraction2);
         friend bool operator<(const Fraction& fraction1, const Fraction& fraction2);
         friend bool operator>=(const Fraction& fraction1, const Fraction& fraction2);
         friend bool operator<=(const Fraction& fraction1, const Fraction& fraction2);
 
-        friend bool operator==(const Fraction& fraction1, const float num1);
-        friend bool operator!=(const Fraction& fraction1, const float num1);
-        friend bool operator>(const Fraction& fraction1, const float num1);
-        friend bool operator<(const Fraction& fraction1, const float num1);
-        friend bool operator>=(const Fraction& fraction1, const float num1);
-        friend bool operator<=(const Fraction& fraction1, const float num1);
-
-        friend bool operator==(const float num1, const Fraction& fraction1);
-        friend bool operator!=(const float num1, const Fraction& fraction1);
-        friend bool operator>(const float num1, const Fraction& fraction1);
-        friend bool operator<(const float num1, const Fraction& fraction1);
-        friend bool operator>=(const float num1, const Fraction& fraction1);
-        friend bool operator<=(const float num1, const Fraction& fraction1);
+        friend bool operator==(const Fraction& fraction1, float num1);
+        friend bool operator!=(const Fraction& fraction1, float num1);
+        friend bool operator>(const Fraction& fraction1, float num1);
+        friend bool operator<(const Fraction& fraction1, float num1);
+        friend bool operator>=(const Fraction& fraction1, float num1);
+        friend bool operator<=(const Fraction& fraction1, float num1);
 
         Fraction& operator++(); // prefix
         Fraction operator++(int); // postfix
         Fraction& operator--(); // prefix
         Fraction operator--(int); // postfix
 
-        friend istream& operator>>(istream& is, Fraction& fraction);
-        friend ostream& operator<<(ostream& os, const Fraction& fraction);//good
-
-        // conversion constructor
-        Fraction(int num): Fraction(num, 1) { }
-
-        operator double() const {
-            cout << "converting Fraction to double" << endl;
-            return double(this->numerator) / double(this->denominator);
-        }
-        
-        operator string() const {
-            return to_string(this->numerator)+"/"+to_string(this->denominator);
-        }
+        friend istream& operator>>(istream& Istream, Fraction& fraction);
+        friend ostream& operator<<(ostream& Ostream, const Fraction& fraction);
 
     };
 }
